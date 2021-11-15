a9caa40fb0a416f0b17c9b9e100103b397ca3b59
Michael Tang <tngmichael28@gmail.com>
2021-11-14 20:57:55 -0800

    Completed javascript arrays prep.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,22 @@
+const languages = [
+  'html',
+  'css',
+  'javascript',
+  'react',
+  'node',
+  'postgres'
+];
+
+console.log(languages);
+
+var firstElement = languages[0];
+console.log('The first element of the array is ' + firstElement);
+
+var thirdElement = languages[2];
+console.log('The third element of the array is ' + thirdElement);
+
+var length = languages.length;
+console.log('The length of the array is ' + length);
+
+var lastElement = languages[length - 1];
+console.log('The last element of the array is ' + lastElement);
