cdbc54329ef4e2f0a530f91bd3d95dedfdf1f9ca
Michael Tang <tngmichael28@gmail.com>
2021-11-18 16:40:13 -0800

    Completed for loops prep.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,13 @@
+// create your loops here.
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (var i = 0; i < 10; i++) {
+  console.log(i * 2);
+}
+
+for (var i = 100; i > 0; i--) {
+  console.log('Time till explosion ' + i + '!');
+}
+console.log('BOOOOOOMM!');
